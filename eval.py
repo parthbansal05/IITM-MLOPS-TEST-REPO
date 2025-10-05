9eb846bde171bca86ea102a414a04cb596a0f174
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2025-10-05 22:29:43 +0530

    update

--- eval.py
+++ eval.py
@@ -7,7 +7,7 @@ model = joblib.load('svm_iris_model.joblib')
 
 csv_file_path = 'data.csv'
 data = pd.read_csv(csv_file_path)
-
+print(data.head())
 _, test_data = train_test_split(data, test_size=0.3, random_state=42)
 
 test_X = test_data[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]
