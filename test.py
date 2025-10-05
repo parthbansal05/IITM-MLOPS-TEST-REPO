c8966e182f80314082040fbd73376d4d8912c15b
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2025-10-05 21:28:10 +0530

    update

---
+++ test.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd 
+from pandas import Series, DataFrame
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris = pd.read_csv("data.csv")
+
+print(iris.head())
+
+
+# importing alll the necessary packages to use the various classification algorithms
+from sklearn.linear_model import LogisticRegression # for Logistic Regression Algorithm
+from sklearn.cross_validation import train_test_split # to split the dataset for training and testing 
+from sklearn.neighbors import KNeighborsClassifier # KNN classifier
+from sklearn import svm # for suport vector machine algorithm
+from sklearn import metrics # for checking the model accuracy
+from sklearn.tree import DecisionTreeClassifier # for using DTA
+
+train, test = train_test_split(iris, test_size=0.3) # our main data split into train and test
+
+
+train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] # taking the training data features
+train_y = train.Species # output of the training data
+
+test_X = test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] # taking test data feature
+test_y = test.Species # output value of the test data
+
+
+model = svm.SVC() # select the svm algorithm
+
+# we train the algorithm with training data and training output
+model.fit(train_X, train_y)
+
+# we pass the testing data to the stored algorithm to predict the outcome
+prediction = model.predict(test_X)
+print('The accuracy of the SVM is: ', metrics.accuracy_score(prediction, test_y)) # we check the accuracy of the algorithm
+#we pass the predicted output by the model and the actual output
